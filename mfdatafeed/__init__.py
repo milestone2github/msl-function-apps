57c3163ee33fb5e6225d3f2e25693c27d217957e
Sunny Chaudhary <87089998+rigs05@users.noreply.github.com>
2025-11-29 16:39:15 +0530

    - changed to relative importing of components as azure function imports each function's root file as module ; other files like 'process_mails_core.py' imports using absolute referencing ; \n- changed 'name' field in bindings to match with function name ; \n- change 'runOnStartup' flag in functions.json to true if required to trigger function execution immediately

--- mfdatafeed/__init__.py
+++ mfdatafeed/__init__.py
@@ -1,8 +1,8 @@
 import azure.functions as func
-
-from process_mails_core import process_mails_core
-from logger_utils import log_custom, log_error, log_start, log_success
 from datetime import datetime, timezone
+
+from .process_mails_core import process_mails_core
+from .logger_utils import log_custom, log_error, log_start, log_success
 
 def run_process_file():
     run_start_time = datetime.now(timezone.utc)
@@ -14,8 +14,8 @@ def run_process_file():
         log_error("process_mails_core", e)
 
 
-def main(timer_req: func.TimerRequest) -> None:
-    if timer_req.past_due:
+def main(mfdatafeed: func.TimerRequest) -> None:
+    if mfdatafeed.past_due:
         log_custom("info", "Timer is past due!")
 
     log_custom("info", "Starting the Email Processing function...")
